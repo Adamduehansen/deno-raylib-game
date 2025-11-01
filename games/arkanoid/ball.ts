a545cd37e582b0a409ce7758e9f6ee881e5abfca
Adam Due Hansen <adamduehan@gmail.com>
2025-11-01 11:30:03 +0100

    arkanoid: updates ball to use collision system.

--- games/arkanoid/ball.ts
+++ games/arkanoid/ball.ts
@@ -1,11 +1,7 @@
 import { Entity } from "@src/entity.ts";
-import {
-  checkCollisionCircleRec,
-  drawCircleV,
-  getScreenWidth,
-  Maroon,
-} from "../../raylib-bindings.ts";
+import { drawCircleV, getScreenWidth, Maroon } from "../../raylib-bindings.ts";
 import { Paddle } from "./paddle.ts";
+import { Body } from "@src/physics.ts";
 
 export class Ball extends Entity {
   static radius = 7;
@@ -22,6 +18,7 @@ export class Ball extends Entity {
       height: Ball.radius,
       width: Ball.radius,
       name: "ball",
+      body: Body.Circle(Ball.radius),
     });
     this.#paddle = paddle;
   }
@@ -44,44 +41,41 @@ export class Ball extends Entity {
     });
   }
 
+  override onCollision(other: Entity): void {
+    if (other.name === "paddle") {
+      this.velocity.y *= -1;
+      this.velocity.x = (this.pos.x - this.#paddle.pos.x) / 5;
+    }
+  }
+
   override update(): void {
     super.update();
-    this.body.x -= Ball.radius / 2;
-    this.body.y -= Ball.radius / 2;
 
     // Update ball position.
     if (this.#active === false) {
-      this.pos.x = this.#paddle.pos.x + Paddle.width / 2;
+      this.pos.x = this.#paddle.pos.x;
       this.pos.y = this.#paddle.pos.y - 20;
     }
 
-    // Check collision with top
-    if (this.pos.y < 0) {
+    // // Check collision with top
+    if (this.pos.y < 0 + Ball.radius / 2) {
       this.velocity.y *= -1;
     }
 
-    // Check collision with walls
+    // // Check collision with walls
     if (this.pos.x < 0 || this.pos.x > getScreenWidth()) {
       this.velocity.x *= -1;
     }
 
-    // Check collision with paddle
-    if (checkCollisionCircleRec(this.pos, Ball.radius, this.#paddle.body)) {
-      const paddleCenter = this.#paddle.pos.x + Paddle.width / 2;
+    // // Check collision with any brick
+    // const bricks = this.scene?.entityManager.getByName("brick") ?? [];
+    // for (const brick of bricks) {
+    //   if (!checkCollisionCircleRec(this.pos, Ball.radius, brick.body)) {
+    //     continue;
+    //   }
 
-      this.velocity.y *= -1;
-      this.velocity.x = (this.pos.x - paddleCenter) / 5;
-    }
-
-    // Check collision with any brick
-    const bricks = this.scene?.entityManager.getByName("brick") ?? [];
-    for (const brick of bricks) {
-      if (!checkCollisionCircleRec(this.pos, Ball.radius, brick.body)) {
-        continue;
-      }
-
-      this.velocity.y *= -1;
-      brick.remove();
-    }
+    //   this.velocity.y *= -1;
+    //   brick.remove();
+    // }
   }
 }
